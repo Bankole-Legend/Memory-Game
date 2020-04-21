794ae3f28095fff3014b2400851b59c88a9bb817
Bankole_Adefemi <bankoledavidadefemi@gmail.com>
2020-04-21 22:09:49 +0100

    first steps for the game

---
+++ game.js
@@ -0,0 +1,85 @@
+document.addEventListener('DOMCotentLoaded', () =>{
+    //All the boxes representing the the cards
+        const boxArray = [
+            {
+                name: 'first',
+                image: 'images/first.png'
+            },
+            {
+                name: 'first',
+                image: 'images/first.png'
+            },
+            {
+                name: 'second',
+                image: 'images/second.png'
+            },
+            {
+                name: 'second',
+                image: 'images/second.png'
+            },
+            {
+                name: 'third',
+                image: 'images/third.png'
+            },
+            {
+                name: 'third',
+                image: 'images/third.png'
+            },
+            {
+                name: 'fourth',
+                image: 'images/fourth.png'
+            },
+            {
+                name: 'fourth',
+                image: 'images/fourth.png'
+            },
+            {
+                name: 'fifth',
+                image: 'images/fifth.jpeg'
+            },
+            {
+                name: 'fifth',
+                image: 'images/fifth.jpeg'
+            },
+            {
+                name: 'sixth',
+                image: 'images/sixth.png'
+            },
+            {
+                name: 'sixth',
+                image: 'images/sixth.png'
+            },
+        ] 
+    }
+    
+    const gameBox = document.querySelector('.game')
+    // making the game board
+    function gameBoard(){
+        for(let i = 0; i < boxArray.length; i++){
+            var box = document.createElement('img')
+            box.setAttribute('src', 'images')
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    )
+    
